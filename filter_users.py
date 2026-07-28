26079af481adff340ebe543948780aa074b397c2
Saskia Heidebring <s.heidebring@web.de>
2026-07-28 12:36:37 +0200

    Adds options to filter by email or age

--- filter_users.py
+++ filter_users.py
@@ -28,10 +28,16 @@ def filter_users_by_email(email):
         print(user)
 
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, only 'name' is supported): ").strip().lower()
+    filter_option = input("What would you like to filter by? (name, email, age): ").strip().lower()
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
         filter_users_by_name(name_to_search)
+    elif filter_option == "email":
+        email_to_search = input("Enter a email to filter users: ").strip()
+        filter_users_by_email(email_to_search)
+    elif filter_option == "age":
+        age_to_search = int(input("Enter a age to filter users: ").strip())
+        filter_users_by_age(age_to_search)
     else:
         print("Filtering by that option is not yet supported.")
